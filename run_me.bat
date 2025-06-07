9cf65b6ccf854ad4aea80113440295537b23321f
气球_ba1100n <33679363+0xba1100n@users.noreply.github.com>
2025-06-07 12:52:23 +0800

    Update run_me.bat

--- run_me.bat
+++ run_me.bat
@@ -1,1 +1,3 @@
-powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0Restart-I2CHIDAndSendKey.ps1"
+@echo off
+powershell -NoProfile -ExecutionPolicy Bypass -NoExit -Command "& { $path = '%~dp0Restart-I2CHIDAndSendKey.ps1'; $s = Get-Content -Raw -LiteralPath $path -Encoding UTF8; Invoke-Expression $s }"
+pause
